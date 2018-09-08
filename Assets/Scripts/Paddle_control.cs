dd6cedf5c8c8705eb6f1909f07d2d60c72bd6a3f
Phil Flavin <pflavin@gmail.com>
2018-09-08 18:50:10 +0100

    Walls added and paddle limits implemented.

--- Assets/Scripts/Paddle_control.cs
+++ Assets/Scripts/Paddle_control.cs
@@ -7,6 +7,10 @@ public class Paddle_control : MonoBehaviour {
     private bool movingLeft;
     private bool movingRight;
 
+    public Rigidbody2D leftLimit;
+    public Rigidbody2D rightLimit;
+
+
     public float paddle_speed;
     public int direction;
 
@@ -35,29 +39,40 @@ public class Paddle_control : MonoBehaviour {
         //Debug.Log(direction);
 
         transform.Translate(direction * paddle_speed * Time.deltaTime, 0, 0);
+
+        //Keep Paddle in bounds
+
+        if (transform.position.x <= leftLimit.position.x)
+        {
+            transform.position = new Vector2(leftLimit.position.x, transform.position.y);
+        }
+        if (transform.position.x >= rightLimit.position.x)
+        {
+            transform.position = new Vector2(rightLimit.position.x, transform.position.y);
+        }
     }
 
     public void startLeft()
     {
         movingLeft = true;
-        Debug.Log("start Left");
+        //Debug.Log("start Left");
     }
 
     public void stopLeft()
     {
         movingLeft = false;
-        Debug.Log("stop Left");
+        //Debug.Log("stop Left");
     }
 
     public void startRight()
     {
         movingRight = true;
-        Debug.Log("start Right");
+        //Debug.Log("start Right");
     }
 
     public void stopRight()
     {
         movingRight = false;
-        Debug.Log("stop Right");
+       // Debug.Log("stop Right");
     }
 }
